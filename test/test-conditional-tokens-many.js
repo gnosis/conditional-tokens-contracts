9cf4c70b7623e356305de60a55d6442ee983e645
Victor Porton <porton@narod.ru>
2020-12-07 06:48:42 +0200

    testing

--- test/test-conditional-tokens-many.js
+++ test/test-conditional-tokens-many.js
@@ -24,8 +24,10 @@ contract("ConditionalTokensMany", function(accounts) {
     counterparty
   ] = accounts;
 
-  beforeEach("deploy ConditionalTokens", async function() {
+  beforeEach("initiate token contracts", async function() {
     this.conditionalTokens = await ConditionalTokensMany.new();
+    // this.collateral = await ERC20Mintable.new(); // TODO: Check multiple collaterals
+    // this.collateral.mint(donor, "10000");
   });
 
   describe("createMarket", function() {
@@ -35,39 +37,39 @@ contract("ConditionalTokensMany", function(accounts) {
         this.customer1 = accounts[1];
         this.customer2 = accounts[2];
         ({ logs: this.logs1 } = await this.conditionalTokens.createMarket());
-        this.marketId1 = this.logs1[0].args.marketId;
+        this.market1 = this.logs1[0].args.marketId;
         ({ logs: this.logs2 } = await this.conditionalTokens.createMarket());
-        this.marketId2 = this.logs2[0].args.marketId;
+        this.market2 = this.logs2[0].args.marketId;
       });
 
       it("should emit a MarketCreated event", function() {
-        this.marketId1.should.be.bignumber.equal("0");
-        this.marketId2.should.be.bignumber.equal("1");
+        this.market1.should.be.bignumber.equal("0");
+        this.market2.should.be.bignumber.equal("1");
         expectEvent.inLogs(this.logs1, "MarketCreated", {
           oracle: this.oracle1,
-          marketId: this.marketId1
+          marketId: this.market1
         });
         expectEvent.inLogs(this.logs2, "MarketCreated", {
           oracle: this.oracle1,
-          marketId: this.marketId2
+          marketId: this.market2
         });
       });
 
       it("should leave payout denominator unset", async function() {
         (
-          await this.conditionalTokens.payoutDenominator(this.marketId1)
+          await this.conditionalTokens.payoutDenominator(this.market1)
         ).should.be.bignumber.equal("0");
         (
-          await this.conditionalTokens.payoutDenominator(this.marketId2)
+          await this.conditionalTokens.payoutDenominator(this.market2)
         ).should.be.bignumber.equal("0");
       });
 
       it("should not be able to register the same customer more than once for the same market", async function() {
-        await this.conditionalTokens.registerCustomer(this.marketId1, [], {
+        await this.conditionalTokens.registerCustomer(this.market1, [], {
           from: this.customer1
         });
         await expectRevert(
-          this.conditionalTokens.registerCustomer(this.marketId1, [], {
+          this.conditionalTokens.registerCustomer(this.market1, [], {
             from: this.customer1
           }),
           "customer already registered"
@@ -75,19 +77,50 @@ contract("ConditionalTokensMany", function(accounts) {
         // TODO: Check that can register the same customer for different markets.
       });
 
-      it("cheking the math", async function() {
-        await this.conditionalTokens.registerCustomer(this.marketId1, [], {
-          from: this.customer1
-        });
-        await this.conditionalTokens.registerCustomer(this.marketId1, [], {
+      it("checking the math", async function() {
+        const collateral = await this.conditionalTokens.registerCustomer(
+          this.market1,
+          [],
+          {
+            from: this.customer1
+          }
+        );
+        await this.conditionalTokens.registerCustomer(this.market1, [], {
           from: this.customer2
         });
-        await this.conditionalTokens.registerCustomer(this.marketId2, [], {
+        await this.conditionalTokens.registerCustomer(this.market2, [], {
           from: this.customer1
         });
-        await this.conditionalTokens.registerCustomer(this.marketId2, [], {
+        await this.conditionalTokens.registerCustomer(this.market2, [], {
           from: this.customer2
         });
+
+        await this.conditionalTokens.reportDenominator(this.market1, toBN("3"));
+        await this.conditionalTokens.reportNumerator(
+          this.market1,
+          this.customer1,
+          toBN("20")
+        );
+        await this.conditionalTokens.reportNumerator(
+          this.market1,
+          this.customer2,
+          toBN("10")
+        );
+        await this.conditionalTokens.finishMarket(this.market1);
+        await this.conditionalTokens.reportDenominator(this.market2, toBN("3"));
+        await this.conditionalTokens.reportNumerator(
+          this.market2,
+          this.customer1,
+          toBN("20")
+        );
+        await this.conditionalTokens.reportNumerator(
+          this.market2,
+          this.customer2,
+          toBN("10")
+        );
+        await this.conditionalTokens.finishMarket(this.market2);
+
+        // await collateralBalanceOf(collateralToken, market1, customer1)
         // TODO
       });
     });
