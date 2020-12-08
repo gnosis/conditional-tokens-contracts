78b93ceafdb68ba7f14819bc7681f760172e0354
Victor Porton <porton@narod.ru>
2020-12-08 06:04:11 +0200

    manyid library

---
+++ utils/manyid-helpers.js
@@ -0,0 +1,55 @@
+// To use this, just import this file and supply it with some web3 utils:
+//     require("@gnosis.pm/conditional-tokens-contracts/utils/manyid-helpers")(web3.utils)
+
+const TOKEN_CONDITIONAL = 0;
+const TOKEN_DONATED = 1;
+const TOKEN_STAKED = 2;
+const TOKEN_REDEEMED = 3;
+
+module.exports = function({ soliditySha3 }) {
+  function conditionalTokenId(market, customer) {
+    return soliditySha3(
+      { t: "uint8", v: TOKEN_CONDITIONAL },
+      { t: "uint64", v: market },
+      { t: "address", v: customer }
+    );
+  }
+
+  function collateralDonatedTokenId(collateralToken, market, outcome) {
+    return soliditySha3(
+      { t: "uint8", v: TOKEN_DONATED },
+      { t: "address", v: collateralToken },
+      { t: "uint64", v: market },
+      { t: "uint64", v: outcome }
+    );
+  }
+
+  function collateralStakedTokenId(collateralToken, market, outcome) {
+    return soliditySha3(
+      { t: "uint8", v: TOKEN_STAKED },
+      { t: "address", v: collateralToken },
+      { t: "uint64", v: market },
+      { t: "uint64", v: outcome }
+    );
+  }
+
+  function collateralRedeemedTokenId(collateralToken, market, outcome) {
+    return soliditySha3(
+      { t: "uint8", v: TOKEN_REDEEMED },
+      { t: "address", v: collateralToken },
+      { t: "uint64", v: market },
+      { t: "uint64", v: outcome }
+    );
+  }
+
+  return {
+    conditionalTokenId,
+    collateralDonatedTokenId,
+    collateralStakedTokenId,
+    collateralRedeemedTokenId,
+    TOKEN_CONDITIONAL,
+    TOKEN_DONATED,
+    TOKEN_STAKED,
+    TOKEN_REDEEMED
+  };
+};
