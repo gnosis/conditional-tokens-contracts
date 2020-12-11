d6adda78ab40c8f267705ea8ec4bcc8e986dc29f
Victor Porton <porton@narod.ru>
2020-12-11 21:58:57 +0200

    comment

--- contracts/BaseBidOnAddresses.sol
+++ contracts/BaseBidOnAddresses.sol
@@ -8,6 +8,7 @@ import { IERC1155 } from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 
 // FIXME: Two-level collateral transfer (see `salary` branch) is buggy:
 // If somebody asks to transfer the collateral from a higher level oracle, it may be too late as the lower level may be already redeemed.
+// This can be solved by remebering the amount of each redeemed collateral for each user.
 
 // TODO: Token URL setting.
 /// @title Bidding on Ethereum addresses
