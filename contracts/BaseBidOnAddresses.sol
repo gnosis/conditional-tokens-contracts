6fcc6909f95da38305eed78c58d5ac9cf428a311
Victor Porton <porton@narod.ru>
2020-12-12 04:01:16 +0200

    error in a comment corrected

--- contracts/BaseBidOnAddresses.sol
+++ contracts/BaseBidOnAddresses.sol
@@ -15,7 +15,7 @@ import { IERC1155 } from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 /// @title Bidding on Ethereum addresses
 /// @author Victor Porton
 /// @notice Not audited, not enough tested.
-/// This allows anyone claim 1000 conditional tokens in order for him to transfer money from the future.
+/// This allows anyone claim conditional tokens in order for him to transfer money from the future.
 /// See `docs/future-money.rst`.
 ///
 /// We have three kinds of ERC-1155 token ID
