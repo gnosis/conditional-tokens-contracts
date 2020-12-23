edbd65a2b7b6fe7c11507a545d257e347ea9af02
Victor Porton <porton@narod.ru>
2020-12-23 19:33:24 +0200

    doc comment

--- contracts/BidOnAddresses.sol
+++ contracts/BidOnAddresses.sol
@@ -8,7 +8,7 @@ import { ERC1155WithTotals } from "./ERC1155/ERC1155WithTotals.sol";
 /// @author Victor Porton
 /// @notice Not audited, not enough tested.
 /// This allows anyone claim 1000 conditional tokens in order for him to transfer money from the future.
-/// See `docs/future-money.rst`.
+/// See `docs/future-money.rst` and anyone to donate or bequest.
 ///
 /// We have three kinds of ERC-1155 token ID
 /// - a combination of market ID, collateral address, and customer address (conditional tokens)
